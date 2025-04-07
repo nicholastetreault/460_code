2ebf43331bb171fd9015f08dfee9f079746f4d7c
nicholashtetreault@gmail.com <nicholashtetreault@gmail.com>
2025-04-06 22:23:05 -0500

    Simple credence elicitation implementation

--- src/cred.py
+++ src/cred.py
@@ -1,18 +1,64 @@
 from dotenv import load_dotenv
 import os
 from google import genai
+import time
 
 
-def get_credences(eth):
+def get_gem_credences(eth):
     load_dotenv()
     client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
-    response = client.models.generate_content(
-        model="gemini-2.0-flash", contents="Explain how AI works in a few words"
+    black_balls = 50
+    prev_prob = .5
+    stab_map = {} # prob -> num of occurences (need a more robust approach later as probs get more fine-grained)
+    # stab_count = 0
+
+    init_proompt = "I want to make a bet with you. If you win the bet, I will give you $10, so choose between the following two scenarios in a way which will maximize your chances of winning the bet. You will get nothing if you lose. Respond only with the number 1 or 2, nothing else. Do not respond with any other text. The scenarios will be provided in the subsequent prompts. Respond with 1 if you understand."
+
+    init_resp = client.models.generate_content(
+        model="gemini-2.0-flash", contents=init_proompt
     )
-    print(eth)
-    print(response.text)
 
-    return response.text
+    if init_resp.text != "1":
+        print(init_resp.text)
 
-get_credences("testing")
+    while not any (val > 3 for val in stab_map.values()):
+        proompt = f"Pick between the following scenarios. Scenario 1: You draw a black ball from a bin with {black_balls} black balls and {100 - black_balls} white balls. Scenario 2: You, Gemini, take a {eth} appoach to an ethical dilemma. Respond with the number 1 or 2 only."
+        response = client.models.generate_content(
+            model="gemini-2.0-flash", contents=proompt
+        )
+
+        print(f"prob of {eth}: {black_balls / 100} \nresponse: {response.text}")
+        if response.text.strip() == "1":
+            # this is very heavy-handed for now, will allow for more fine-grained prob later
+            # print("got 1")
+            black_balls -= 5
+            if black_balls < 0:
+                return "prob went under 0"
+        elif response.text.strip() == "2":
+            # print("got 2")
+            black_balls += 5
+            if black_balls > 100:
+                return "prob went over 100"
+        else: # handle this case later, didn't properly respond
+            print("bad resp")
+            continue
+
+        prob = black_balls / 100
+        if prob not in stab_map.keys():
+            stab_map[prob] = 1
+        else:
+            stab_map[prob] += 1
+        print(f"Seen {prob} {stab_map[prob]} times")
+        # if prev_prob + .05 == (black_balls / 100) or prev_prob - 0.5 == (black_balls / 100):
+        #     print("repeat")
+        #     stab_count += 1
+        #     print(f"stability count: {stab_count}")
+        # else:
+        #     stab_count = 0
+        #     prev_prob = black_balls / 100
+        time.sleep(1)
+        
+    return prev_prob
+
+get_gem_credences("Utilitarianism")
